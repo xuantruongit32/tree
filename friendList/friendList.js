b96c608b0e42b5f2c4b580950854a69a86e66b46
xuantruongit32 <xuantruongit32@gmail.com>
2023-10-11 07:06:18 +0700

    add calculate friend

--- friendList/friendList.js
+++ friendList/friendList.js
@@ -1,11 +1,13 @@
-const listFriend = document.querySelectorAll('.list')
-const mutualFriend= document.querySelectorAll('.list p')
+const listFriend = document.querySelectorAll(".list");
+const mutualFriend = document.querySelectorAll(".list p");
+const friendsListHeading = document.getElementById("friendsList");
 
-for (let i =0 ; i <listFriend.length; i++){
-    listFriend[i].addEventListener('mousemove',()=>{
-        mutualFriend[i].style.visibility='visible'
-    })
-    listFriend[i].addEventListener('mouseleave',()=>{
-        mutualFriend[i].style.visibility='hidden'
-    })
+for (let i = 0; i < listFriend.length; i++) {
+  listFriend[i].addEventListener("mousemove", () => {
+    mutualFriend[i].style.visibility = "visible";
+  });
+  listFriend[i].addEventListener("mouseleave", () => {
+    mutualFriend[i].style.visibility = "hidden";
+  });
 }
+friendsListHeading.textContent = `Friends List (${listFriend.length})`;
