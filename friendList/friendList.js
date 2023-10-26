a75ae441125f37519d0baa258a2550adfc04b079
xuantruongit32 <xuantruongit32@gmail.com>
2023-10-26 13:18:39 +0700

    add search friend

--- friendList/friendList.js
+++ friendList/friendList.js
@@ -149,6 +149,43 @@ for (let i = 0; i < listFriend.length; i++) {
   });
 }
 friendsListHeading.textContent = `Friends List (${listFriend.length})`;
+const friendSearch = document.querySelector(".container .search input");
+
+function updateFriendList() {
+  const searchText = friendSearch.value.toLowerCase();
+
+  const updateFriend = friendsData.filter((friend) =>
+    friend.name.toLowerCase().includes(searchText)
+  );
+
+  box.innerHTML = "";
+
+  updateFriend.forEach((friend, index) => {
+    const listItem = createFriend(
+      friend.name,
+      friend.mutualFriends,
+      friend.imageSrc,
+      index + 1
+    );
+    box.appendChild(listItem);
+  });
+
+const listFriend = document.querySelectorAll(".list");
+const mutualFriend = document.querySelectorAll(".list p");
+const friendsListHeading = document.getElementById("friendsList");
+
+for (let i = 0; i < listFriend.length; i++) {
+  listFriend[i].addEventListener("mousemove", () => {
+    mutualFriend[i].style.visibility = "visible";
+  });
+  listFriend[i].addEventListener("mouseleave", () => {
+    mutualFriend[i].style.visibility = "hidden";
+  });
+}
+}
+
+friendSearch.addEventListener("input", updateFriendList);
+
 
 const home = document.querySelector(' .header .main .home');
 const friend=document.querySelector(' .header .main .friend');
