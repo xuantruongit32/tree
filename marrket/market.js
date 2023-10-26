2de2970e7c570631a1face55d0157d86d204cfbb
Tung <leethanhtung250702@gmail.com>
2023-10-26 16:50:05 +0700

    change

--- marrket/market.js
+++ marrket/market.js
@@ -33,30 +33,7 @@ function removeFromCart(productTitle) {
 }
 
 // Update the cart display
-function updateCartDisplay() {
-  cartContent.innerHTML = "";
-  cartTotal = 0;
-  
-  cartItems.forEach(item => {
-    const cartItem = document.createElement("div");
-    cartItem.classList.add("cart-box");
-    cartItem.innerHTML = `
-      <img src="img/messi.jpg.img" alt="" class="cart-img">
-      <div class="detail-box">
-        <div class="cart-product-title">${item.title}</div>
-        <div class="cart-price">${item.price.toFixed(2)}VND</div>
-        <input type="number" value="${item.quantity}" class="cart-quantity">
-      </div>
-      <i class='bx bx-trash cart-remove' data-title="${item.title}"></i>
-    `;
-    
-    cartContent.appendChild(cartItem);
-    
-    cartTotal += item.price * item.quantity;
-  });
-  
-  totalPrice.textContent = `$${cartTotal.toFixed(2)}`;
-}
+
 
 // Event listeners
 cartIcon.addEventListener("click", () => {
@@ -90,3 +67,71 @@ cartContent.addEventListener("click", (e) => {
 });
 
 updateCartDisplay();
+
+// Thêm sự thay đổi cho hàm addToCart
+function addToCart(productBox) {
+  const productTitle = productBox.querySelector(".product-title").textContent;
+  const productPrice = parseFloat(productBox.querySelector(".price").textContent);
+  const productImg = productBox.querySelector(".product-img").getAttribute("src"); // Sửa lại đường dẫn ảnh
+
+  const existingItem = cartItems.find(item => item.title === productTitle);
+
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cartItems.push({ title: productTitle, price: productPrice, quantity: 1, img: productImg });
+  }
+
+  updateCartDisplay();
+}
+
+// Thay đổi hàm updateCartDisplay để hiển thị hình ảnh sản phẩm
+function updateCartDisplay() {
+  cartContent.innerHTML = "";
+  cartTotal = 0;
+
+  cartItems.forEach(item => {
+    const cartItem = document.createElement("div");
+    cartItem.classList.add("cart-box");
+    cartItem.innerHTML = `
+      <img src="${item.img}" alt="" class="cart-img">
+      <div class="detail-box">
+        <div class="cart-product-title">${item.title}</div>
+        <div class="cart-price">${item.price.toFixed(2)}VND</div>
+        <input type="number" value="${item.quantity}" class="cart-quantity">
+      </div>
+      <i class='bx bx-trash cart-remove' data-title="${item.title}"></i>
+    `;
+
+    cartContent.appendChild(cartItem);
+
+    cartTotal += item.price * item.quantity;
+  });
+
+  totalPrice.textContent = ` $${cartTotal.toFixed(2)}`;
+}
+const home = document.querySelector(' .header .main .home');
+const friend=document.querySelector(' .header .main .friend');
+const market = document.querySelector(' .header .main .market');
+const story= document.querySelector(' .header .main .story');
+const personal=document.querySelector('.header .right .avatar');
+
+const changePage = ()=>{
+    home.addEventListener('click',()=>{
+        document.location.href='../newFeed/newfeed.html'
+    });
+    friend.addEventListener('click',()=>{
+        document.location.href='../friendList/friendList.html'
+    });
+     market.addEventListener('click',()=>{
+         document.location.href='../marrket/market.html'
+     })
+    story.addEventListener('click',()=>{
+        document.location.href='../story/story.html'
+    });
+  
+    personal.addEventListener('click',()=>{
+        document.location.href='../personalPage/personPage.html'
+    });
+}
+changePage();
